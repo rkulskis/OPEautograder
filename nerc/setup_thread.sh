65353230c013764d94e6ac5fb103be9222dd107a
rkulskis <rkulskis@gmail.com>
2024-03-10 20:42:03 -0400

    correct paths

--- nerc/setup_thread.sh
+++ nerc/setup_thread.sh
@@ -9,6 +9,8 @@ mkdir -p $THREAD_DIR/autograder/{source,submission,results}
 
 cd ${THREAD_DIR}/autograder/source
 cp -r /app/assignments/${REPO_PATH_TO_ASSIGNMENTS}/${ASSIGNMENT_NAME}/autograder/* .
+rm -rf tests
+cp -r /app/assignments/${REPO_PATH_TO_ASSIGNMENTS}/${ASSIGNMENT_NAME}/tests .
 chmod +x run_autograder
 
 find . -type f -exec sed -i "s#/autograder#${THREAD_DIR}/autograder#g" {} +
